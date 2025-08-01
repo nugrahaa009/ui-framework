870f943beaa460575836ae96c85b757d62fe1169
msbu-aditya.nugraha <msbu-aditya.nugraha@edot.id>
2025-08-01 16:39:11 +0700

    last update

--- dist/src/components/Layout/Layout.d.ts
+++ dist/src/components/Layout/Layout.d.ts
@@ -1,12 +1,14 @@
-import { Layout as AntLayout } from 'antd';
-export declare const Header: React.FC<React.ComponentProps<typeof AntLayout.Header>>;
-export declare const Content: React.FC<React.ComponentProps<typeof AntLayout.Content>>;
-export declare const Footer: React.FC<React.ComponentProps<typeof AntLayout.Footer>>;
-export declare const Sider: React.FC<React.ComponentProps<typeof AntLayout.Sider>>;
-export declare const Layout: import("react").ForwardRefExoticComponent<import("antd").LayoutProps & import("react").RefAttributes<HTMLElement>> & {
-    Header: import("react").ForwardRefExoticComponent<import("antd").LayoutProps & import("react").RefAttributes<HTMLElement>>;
-    Footer: import("react").ForwardRefExoticComponent<import("antd").LayoutProps & import("react").RefAttributes<HTMLElement>>;
-    Content: import("react").ForwardRefExoticComponent<import("antd").LayoutProps & import("react").RefAttributes<HTMLElement>>;
-    Sider: import("react").ForwardRefExoticComponent<import("antd").SiderProps & import("react").RefAttributes<HTMLDivElement>>;
+import React from 'react';
+declare const AntHeader: React.ForwardRefExoticComponent<import("antd").LayoutProps & React.RefAttributes<HTMLElement>>, AntContent: React.ForwardRefExoticComponent<import("antd").LayoutProps & React.RefAttributes<HTMLElement>>, AntFooter: React.ForwardRefExoticComponent<import("antd").LayoutProps & React.RefAttributes<HTMLElement>>, AntSider: React.ForwardRefExoticComponent<import("antd").SiderProps & React.RefAttributes<HTMLDivElement>>;
+export declare const Header: React.FC<React.ComponentProps<typeof AntHeader>>;
+export declare const Content: React.FC<React.ComponentProps<typeof AntContent>>;
+export declare const Footer: React.FC<React.ComponentProps<typeof AntFooter>>;
+export declare const Sider: React.FC<React.ComponentProps<typeof AntSider>>;
+export declare const BaseLayout: React.ForwardRefExoticComponent<import("antd").LayoutProps & React.RefAttributes<HTMLElement>> & {
+    Header: React.ForwardRefExoticComponent<import("antd").LayoutProps & React.RefAttributes<HTMLElement>>;
+    Footer: React.ForwardRefExoticComponent<import("antd").LayoutProps & React.RefAttributes<HTMLElement>>;
+    Content: React.ForwardRefExoticComponent<import("antd").LayoutProps & React.RefAttributes<HTMLElement>>;
+    Sider: React.ForwardRefExoticComponent<import("antd").SiderProps & React.RefAttributes<HTMLDivElement>>;
     _InternalSiderContext: typeof import("antd/es/layout/Sider").SiderContext;
 };
+export {};
--- dist/src/components/Layout/Layout.js
+++ dist/src/components/Layout/Layout.js
@@ -1,15 +1,8 @@
 import { jsx as _jsx } from "react/jsx-runtime";
 import { Layout as AntLayout } from 'antd';
-export const Header = (props) => {
-    return _jsx(AntLayout.Header, Object.assign({}, props));
-};
-export const Content = (props) => {
-    return _jsx(AntLayout.Content, Object.assign({}, props));
-};
-export const Footer = (props) => {
-    return _jsx(AntLayout.Footer, Object.assign({}, props));
-};
-export const Sider = (props) => {
-    return _jsx(AntLayout.Sider, Object.assign({}, props));
-};
-export const Layout = AntLayout;
+const { Header: AntHeader, Content: AntContent, Footer: AntFooter, Sider: AntSider } = AntLayout;
+export const Header = (props) => (_jsx(AntHeader, Object.assign({}, props)));
+export const Content = (props) => (_jsx(AntContent, Object.assign({}, props)));
+export const Footer = (props) => (_jsx(AntFooter, Object.assign({}, props)));
+export const Sider = (props) => (_jsx(AntSider, Object.assign({}, props)));
+export const BaseLayout = AntLayout;
